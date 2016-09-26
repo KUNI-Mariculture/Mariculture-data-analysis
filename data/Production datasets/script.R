c960f87b33beef202c423a919aa6f8b7dc560c7b
owenrliu <owenrliu@gmail.com>
2016-09-26 16:24:52 -0700

    finished normalization

--- data/Production datasets/script.R
+++ data/Production datasets/script.R
@@ -14,8 +14,8 @@ Aquaculture$year <- as.numeric(gsub( "X", "", as.character(Aquaculture$year)))
 
 AQ = ddply(Aquaculture, .(year,country),
              summarize, 
-             a.production = sum(production,na.rm=TRUE))
-
+             a.production = sum(production,na.rm=TRUE),
+             a.species = length(unique(species,na.rm=TRUE)))
 Fisheries <- read.csv("~/Dropbox/Proyectos/Aquaculture/Production datasets/capture_production_1950_2014.csv")
 
 Fisheries <- melt(Fisheries, id.vars=c('country','species','f.area','measure'),var='year')
@@ -25,11 +25,12 @@ Fisheries$year <- as.numeric(gsub( "X", "", as.character(Fisheries$year)))
 
 FS = ddply(Fisheries, .(year,country),
          summarize, 
-         f.production = sum(production,na.rm=TRUE))
+         f.production = sum(production,na.rm=TRUE),
+         f.species = length(unique(species,na.rm=TRUE)))
 
 PD <- merge(FS,AQ,by.x=c('year','country'),by.y=c('year','country'),all="TRUE")
-PD$ratio <- PD$a.production/PD$f.production
-
+PD$p.ratio <- PD$a.production/PD$f.production
+PD$p.species <- PD$a.species/PD$f.species
 
 write.csv(PD,'PD.csv')
 
