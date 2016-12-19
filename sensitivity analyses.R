cbd858e454259693afed3ae71399a8a5d537fbe6
owenrliu <owenrliu@gmail.com>
2016-12-19 08:50:56 -0800

    remove-one sensitivity analyses

--- sensitivity analyses.R
+++ sensitivity analyses.R
@@ -3,7 +3,7 @@
 library(dplyr)
 library(ggplot2)
 
-dat.norm <- read.csv(paste0(getwd(),"/mariculture_opportunity_metrics_10.23.16.csv"))
+dat.norm <- read.csv(paste0(getwd(),"/mariculture_opportunity_metrics_12.16.16.csv"))
 dat.new <- dat.norm %>% select(country,contains("_norm")) #just the normalized variables
 
 # variables to test, and categories of variables
@@ -24,6 +24,8 @@ calc_scores <- function(dat) {
   out$mean_econ <- rowMeans(select(out,one_of(econvars)),na.rm=T)
   out$mean_nutrition <- rowMeans(select(out,one_of(nutvars)),na.rm=T)
   out$mean_nutrition[is.nan(out$mean_nutrition)] <- NA
+  #nutrition has to be normalized
+  out$mean_nutrition <- (out$mean_nutrition-min(out$mean_nutrition,na.rm=T))/(max(out$mean_nutrition,na.rm=T)-min(out$mean_nutrition,na.rm=T))
   out$mean_reliance <- rowMeans(select(out,one_of(reliancevars)),na.rm=T)
   out <- out %>% mutate(econ_opportunity=1-mean_econ,mean_malnutrition=1-mean_nutrition)
   out$mariculture_opportunity <- rowMeans(select(out,mean_reliance,econ_opportunity,mean_malnutrition))
@@ -41,13 +43,9 @@ calc_diffs <- function(dat.full,dat.reduced,title) {
   
   hist(econdiff,main="Econ Change",xlim=c(-0.5,0.5))
   hist(nutdiff,main="Nutrition Change",xlim=c(-0.5,0.5))
-  hist(reliancediff,main="Rel Change",xlim=c(-0.5,0.5))
-  hist(oppdiff,main="Opp change",xlim=c(-0.5,0.5))
+  hist(reliancediff,main="Reliance Change",xlim=c(-0.5,0.5))
+  hist(oppdiff,main="TotalOpp Change",xlim=c(-0.5,0.5))
   title(title,outer=T)
-  #out <- c(econdiff,nutdiff,reliancediff,oppdiff)
-  
-  # names(out) <- c("econdiff","nutdiff","reliancediff","oppdiff")
-  # return(out)
 }
 
 # test for removing one variable
@@ -57,6 +55,6 @@ setwd(paste0(getwd(),"/sensitivity data"))
 for(i in 1:length(testvars)) {
   filename <- paste0(testvars[i],"_sens.png")
   png(filename)
-  dat.new[-i] %>% calc_scores(.) %>% calc_diffs(dat.norm,.,title=testvars[i])
+  dat.new[-i] %>% calc_scores(.) %>% calc_diffs(dat.norm,.,title=paste(testvars[i], "removed"))
   dev.off()
 }
